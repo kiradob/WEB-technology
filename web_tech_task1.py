52cc0ed79bf20303a58da8e708b9fcba95420889
kira.dob <arik.dobrynina@yandex.ru>
2023-03-30 07:38:08 +0700

    added web tech task1

---
+++ web_tech_task1.py
@@ -0,0 +1,8 @@
+# Задание 1
+# Открыть в браузере ссылки, определить по какому протоколу HTTP или HTTPS работают сайты:
+# 1. google.ru - HTTPS
+# 2. botanicgarden.ru - HTTPS
+# 3. tinkoff.ru - HTTPS
+# 4. alieexpress.ru - HTTPS
+# 5. priut-koshek.ru - HTTP (никакой личной информации не хранится)
+
